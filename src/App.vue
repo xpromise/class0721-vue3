1fcfb7845a83f0a1d44fb83a406f585142e021de
xiongjian <webjsforyou@gmail.com>
2020-12-09 16:05:14 +0800

    feat: vue3的composition API

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>count: {{count}}</p>
+  <p>{{num}}</p>
+  <!-- <button @click="count++">按钮</button> -->
+  <button @click="handleClick">按钮</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {reactive, toRefs, onMounted, onBeforeUnmount, onBeforeUpdate, ref} from 'vue'
+
+/*
+  https://vue3js.cn/docs/zh/guide/migration/introduction.html#%E6%A6%82%E8%A7%88
+
+  setup() composition API入口 (在beforeCreate和created调用的)
+  reactive() 定义响应式数据的方法
+  toRefs() 展开响应式数据，可以单个使用
+  生命周期
+*/
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // data() {
+  //   return {
+  //     count: 0
+  //   }
+  // }
+  // composition API入口
+  setup() {
+
+    console.log('setup', this); // 没有this
+    
+    // 定义单个响应式数据
+    const num = ref(1)
+    // 定义多个响应式数据
+    const state = reactive({
+      count: 0
+    })
+    
+    const handleClick = () => {
+      state.count++;
+    }
+
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate');
+    })
+
+    onMounted(() => {
+      console.log('onMounted');
+    })
+
+    onBeforeUnmount(() => {
+      console.log('onBeforeUnmount');
+    })
+    // console.log(toRef(state.count));
+    // 返回值对象，会作为数据添加到this上
+    return {
+      // state,
+      // count: toRef(state.count),
+      ...toRefs(state), 
+      handleClick,
+      num
+    }
+
   }
 }
 </script>
